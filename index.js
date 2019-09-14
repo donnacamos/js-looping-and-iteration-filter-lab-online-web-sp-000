3f8838745f44baa686a24a8df6762cf372ca7a57
Donna Amos <donnacamos.developer@gmail.com>
2019-09-14 22:42:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 function findMatching(drivers, name){
   return drivers.filter(n => n.toUpperCase() === name.toUpperCase())
 }
+
+function fuzzyMatch(drivers, query){
+  return drivers.filter(function(n){
+    return query.toUpperCase() === n.slice(0, query.length).toUpperCase();
+  })
+}
